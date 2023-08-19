f7d1c94bf697d733e660229e8a0b7e7669037c2b
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-08-19 12:04:21 +0530

    mohanverse

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import img1 from './assets/pexels-photo-1377034.jpeg'
 
 function Home() {
+  const divStyle = {
+    width: '100%',
+    height: '90vh',
+    backgroundImage: `url(${img1})`,
+    backgroundSize: 'cover',
+    backgroundPosition: '69%',
+};
   return (
     <>
     <Navbar/>
+    <div style={divStyle} >  
+      <div className='md:flex'>
+        <div className='flex ml-10  md:text-4xl space-y-4 md:w-1/2'>
+          <div className=' md:ml-10 mt-40 md:text-4xl space-y-4 text-xl md:font-normal font-bold font-Aclonica'>
+              <h1>STUDIO FIX SPF 15</h1>
+              <h1     className='text-white'>OUR     #1, 24 HR, BUILDABLE,</h1>
+              <p>MEDIUM-TO-FULL COVERAGE     FOUNDATION</p>
+              <p>BREATHABLE NATURAL MATTE FOUNDATION</p>
+          </div>
+        </div>
+        <div className='md:mt-32 md:ml-40 font-Aclonica'>
+            <div className='bg-black rounded-full w-40 text-white h-40 text-center ml-20 mt-4'>
+                <div className='py-10 text-xl'>
+                    <h1>STARTING</h1>
+                    <p>AT ₹1900</p>
+                </div>
+            </div>
+            <div className='bg-black rounded-full w-40 text-white h-40 text-center ml-40 mt-4 '>
+                <div className='p-6'>
+                    <h1>98%</h1>
+                    <p>SAID THIS POUNDATIONS IS NON-CAKEY</p>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 import {GiHamburgerMenu} from 'react-icons/gi'
 import {MdOutlineClose} from 'react-icons/md'
 import {BsFillEmojiSmileFill} from 'react-icons/bs'
-const myimg ='https://previews.123rf.com/images/scyther5/scyther52010/scyther5201000008/156548267-beauty-background-with-facial-cosmetic-products-with-empty-copy-space-makeup-skin-care-concept.jpg'
 
 function Navbar() {
     const [display, setDisplay]=useState('hidden');
@@ -14,46 +13,48 @@ function Navbar() {
         else
         setDisplay('hidden');
     }
-
-    const divStyle = {
-        width: '100%',
-        height: '15vh',
-        backgroundImage: `url(${myimg})`,
-        backgroundSize: 'cover',
-        backgroundPosition: '69%',
-    };
   return (
     <>
-    <div style={divStyle}>
-    <div className='relative mx-10 md:mx-20'>
-         <div className='flex justify-between py-8 font-bold'>
-            <div className='flex text-4xl font-extrabold space-x-1 shadow-2xl'>
-                <BsFillEmojiSmileFill className='mt-1'/>
-                <a className='' href="">GamBly</a>
-            </div>
-           <div className='md:flex hidden shadow-2xl shadow-red-100 space-x-3 md:space-x-6 text-md text-white py-2'>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/hero">Hero</Link>
-                <button className='bg-black text-white px-6 p-1'>Login</button>
-                <button className='bg-black text-white px-6 p-1'>Sign Up</button>
-            </div>
-
-            {display==='hidden'?   <GiHamburgerMenu className='text-3xl md:hidden block' onClick={clickHandle}/> : <MdOutlineClose  className='text-3xl md:hidden block' onClick={clickHandle}/>}
-
-            {/* mobile view */}
-            <div className={`md:hidden ${display} flex-col items-center z-10 py-10 gap-y-4 mt-16 bg-black text-white absolute w-full`}>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/hero">Hero</Link>
-                <button className='bg-white rounded-sm text-black px-6 p-1'>Login</button>
-            </div>
-         </div>
-    </div>
-    <div>
-    </div>
+    <div className='shadow-2xl ticky top-0 sticky bg-white'>
+        <div className='relative mx-10 md:mx-20 font-Aclonica'>
+             <div className='flex justify-between py-4 '>
+                <div className='flex text-4xl font-extrabold space-x-1'>
+                    <BsFillEmojiSmileFill className='text-[#f87171]'/>
+                    <a className='' href="">GamBly</a>
+                </div>
+               <div className='md:flex hidden space-x-3 md:space-x-6 text-md  py-2'>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                    <Link to="/hero">Hero</Link>
+                    <button className='bg-black text-white px-6 p-1'>Login</button>
+                    <button className='bg-black text-white px-6 p-1'>Sign Up</button>
+                </div>
+    
+                {display==='hidden'?   <GiHamburgerMenu className='text-3xl md:hidden block' onClick={clickHandle}/> : <MdOutlineClose      className='text-3xl md:hidden block' onClick={clickHandle}/>}
+    
+                {/* mobile view */}
+                <div className={`md:hidden ${display} flex-col items-center z-10 py-10 gap-y-4 mt-16 bg-black text-white absolute w-full`}>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                    <Link to="/hero">Hero</Link>
+                    <button className='bg-white rounded-sm text-black px-6 p-1'>Login</button>
+                </div>
+             </div>
+        </div>
+        <div className=''>
+          <div className='md:flex hidden justify-between px-8 md:px-28 py-4 text-base text-white font-bold bg-black space-x-4'>
+              <Link to="/">Eyes</Link>
+              <Link to="/">Lipstick</Link>
+              <Link to="/">Perfume</Link>
+              <Link to="/">Skin</Link>
+              <Link to="/">Cream</Link>
+              <Link to="/">FaceOil</Link>
+              <Link to="/">HairOil</Link>
+              <Link to="/">FaceCream</Link>
+          </div>
+        </div>
     </div>
     </>
   )
