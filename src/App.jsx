1255d739a94d7e1eec6d33cf54a7efa9e4771a38
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-08-22 19:59:50 +0530

    mohanverse

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Home from './Home'
 import { Route, Routes } from 'react-router-dom'
 import Contact from './components/Contact'
 import About from './components/About'
+import User from './components/User'
+import Cardstore from './components/Cardstore'
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/about" element={<About />} />
+      <Route path="/card" element={<Cardstore/>} />
+      <Route path="/user" element={<User/>} />
     </Routes>
     </>
   )
