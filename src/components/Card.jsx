d502983fca478d7b708de2a091a13603d3a9889d
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-08-27 14:53:45 +0530

    MOAHNVERSE

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,23 @@ import React from "react";
 
 const Card = (props) => {
   return (
-    <div className="flex justify-center border py-10 px-5">
-      <div className="h-[400px]">
-          <img className="w-52 ml-10" src={props.image} alt="img" />
-          <div className="h-16">
-              <h1 className="text-xl font-md">{props.name}</h1>
-              <h1 className="text-xl font-md">{props.description}</h1>
-          </div>
-          <div className="flex justify-center mt-10">
-              <button className=" bg-black px-4 py-2 rounded-md text-white">Add to Card</button>
-          </div>
-      </div>
+    <>
+    <div className="border p-2">
+        <div className="space-y-6">
+            <div>
+                <img className="w-[400px] h-[300px]" src={props.image}alt="" />
+            </div>
+            <div className="space-y-2">
+                <h1>{props.name}</h1>
+                <p>{props.description}</p>
+            </div>
+            <div className="flex justify-center mt-4">
+                <button className="px-4 py-2 rounded-md text-white bg-black ">add Card</button>
+            </div>
+
+        </div>
     </div>
+    </>
   );
 }
 
